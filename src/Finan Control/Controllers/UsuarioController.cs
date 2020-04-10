12f50c7277706eeed0233018bafb82eb87b6a397
Gabriel Bastos GB. Gabriel <gabriel@dcbtor.com.br>
2020-04-10 04:28:08 -0400

    adicao ninject, criacao listagem a adicao de usuarios

---
+++ src/Finan Control/Controllers/UsuarioController.cs
@@ -0,0 +1,43 @@
+﻿using Finan_Control.DAO;
+using Finan_Control.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Finan_Control.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private UsuarioDAO UsuarioDAO;
+
+        public UsuarioController(UsuarioDAO usuarioDAO)
+        {
+            this.UsuarioDAO = new UsuarioDAO();
+        }
+
+        public ActionResult Index()
+        {
+            return View(UsuarioDAO.Lista());
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Adiciona(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                UsuarioDAO.Adiciona(usuario);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return RedirectToAction("Form");
+            }
+        }
+    }
+}
---
+++ src/Finan Control/DAO/UsuarioDAO.cs
@@ -0,0 +1,30 @@
+﻿using Finan_Control.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Finan_Control.DAO
+{
+    public class UsuarioDAO
+
+    {
+
+        public FinanContext Context { get; set; }
+
+        public UsuarioDAO(FinanContext context)
+        {
+            this.Context = context;
+        }
+        public void Adiciona(Usuario usuario)
+        {
+            Context.Usuarios.Add(usuario);
+            Context.SaveChanges();
+        }
+
+        public IList<Usuario> Lista()
+        {
+            return Context.Usuarios.ToList();
+        }
+    }
+}
